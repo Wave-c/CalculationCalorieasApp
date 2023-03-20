c9285c1e47ffd9fb9dd5900ceb7ea09cbaa31fa5
Админ <@DESKTOP-580DFK1>
2023-03-20 23:34:43 +0300

    начала окно

---
+++ CalculationCalorieasApp/Helpers/EnumDescriptionConverter.cs
@@ -0,0 +1,26 @@
+﻿using CalculationCalorieasApp.Medels.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CalculationCalorieasApp.Helpers
+{
+    public class EnumDescriptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var enm = value as Enum;
+            return enm.GetDescription();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var stringValue = (string)value;
+            return stringValue.ToEnum(targetType);
+        }
+    }
+}
---
+++ CalculationCalorieasApp/Medels/Extensions/EnumExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationCalorieasApp.Medels.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum enm)
+        {
+            FieldInfo fi = enm.GetType().GetField(enm.ToString());
+
+            DescriptionAttribute attribute =
+                fi.GetCustomAttribute(typeof(DescriptionAttribute), false)
+                as DescriptionAttribute;
+
+            return attribute?.Description ?? enm.ToString();
+        }
+
+        public static Enum ToEnum(this string enumDescriptionValue, Type targetType)
+        {
+            var fieldInfos = targetType.GetFields();
+            var fieldsDescriptionsDictionary = fieldInfos.Select(fi =>
+                (fi, (fi.GetCustomAttribute(typeof(DescriptionAttribute), false)
+                as DescriptionAttribute)?.Description))
+                .Where(fieldInfoDescriptionPair =>
+                    fieldInfoDescriptionPair.Description != null)
+                .ToDictionary(keySelector: x => x.Description,
+                    elementSelector: x => (Enum.GetValues(targetType).Cast<Enum>())
+                    .First(enmVal => enmVal.ToString() == x.fi.Name));
+
+            if (fieldsDescriptionsDictionary.TryGetValue(enumDescriptionValue, out var enumValue))
+                return enumValue;
+
+            return default;
+        }
+
+        public static TEnum ToEnum<TEnum>(this string enumDescriptionValue) where TEnum : Enum
+        {
+            return (TEnum)enumDescriptionValue.ToEnum(typeof(TEnum));
+        }
+    }
+}
