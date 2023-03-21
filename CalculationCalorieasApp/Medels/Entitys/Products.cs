790837ad79f0df61d8f1baf18edd9a6fdc9e539d
seval <seval@LISA>
2023-03-21 21:08:52 +0200

    Редактированы кнопки в окнах, довалены Products

---
+++ CalculationCalorieasApp/Medels/Entitys/Products.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CalculationCalorieasApp.Medels.Entitys
+{
+    public class Products:Entity
+    {
+      
+        public string Name;
+        public int Calories;
+        
+
+        public Products(Guid id, string name, int calories)
+        {
+            Id=id;
+            Name = name;
+            Calories = calories;
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Calories}kcal";
+        }
+    }
+}
--- CalculationCalorieasApp/ViewModels/MainWindowViewModel.cs
+++ CalculationCalorieasApp/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,18 @@
 ﻿using CalculationCalorieasApp.Helpers;
 using CalculationCalorieasApp.Medels.Entitys;
+using CalculationCalorieasApp.Views;
 using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Media.Imaging;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
 
 namespace CalculationCalorieasApp.ViewModels
 {
@@ -40,5 +45,68 @@ namespace CalculationCalorieasApp.ViewModels
         {
             Image = await BitmapHelper.SetUserImageAsync(_currentUser);
         }
+
+
+        //public string CheckRadBtnProducts()
+        //{
+        //    string productTime;
+        //    if (RadioButtonBreakfast.IsChecked == true)
+        //    {
+        //        productTime = "Breakfast";
+        //        return productTime;
+        //    }
+        //    else if (RadioButtonDinner.IsChecked == true)
+        //    {
+        //        productTime = "Dinner";
+        //        return productTime;
+        //    }
+        //    else if (RadioButtonSupper.IsChecked == true)
+        //    {
+        //        productTime = "Supper";
+        //        return productTime;
+        //    }
+        //    else
+        //    {
+        //        productTime = "None";
+        //        return productTime;
+        //    }
+
+        //}
+        public void CountCalories()
+        {
+            decimal calories = 0;
+            decimal proteins = 0;
+            decimal carbo = 0;
+            decimal fat = 0;
+            List<Products> breakfast_food = new List<Products>();
+            List<Products> dinner_food = new List<Products>();
+            List<Products> supper_food = new List<Products>();
+            foreach (var item in _breakfeastLbx.Items)
+                breakfast_food.Add((Products)item);
+            foreach (var item in _dinnerLbx.Items)
+                dinner_food.Add((Products)item);
+            foreach (var item in _supperLbx.Items)
+                supper_food.Add((Products)item);
+
+            foreach (var item in breakfast_food)
+            {
+                calories += item.Calories;
+                
+            }
+            foreach (var item in dinner_food)
+            {
+                calories += item.Calories;
+               
+            }
+            foreach (var item in supper_food)
+            {
+                calories += item.Calories;
+               
+            }
+
+            SumCalloriesTxtBl.Text = calories.ToString();
+            
+        }
+
     }
 }
