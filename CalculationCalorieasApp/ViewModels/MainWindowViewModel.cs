e932fe173ea4a0922caba7e85d736b35b128b6fc
seval <seval@LISA>
2023-03-21 21:15:03 +0200

    q

--- CalculationCalorieasApp/ViewModels/MainWindowViewModel.cs
+++ CalculationCalorieasApp/ViewModels/MainWindowViewModel.cs
@@ -40,5 +40,66 @@ namespace CalculationCalorieasApp.ViewModels
         {
             Image = await BitmapHelper.SetUserImageAsync(_currentUser);
         }
+
+
+        //public string CheckRadBtnProducts()
+        //{
+        //    string productTime;
+        //    if (RadioButtonBreakfast.IsChecked == true)
+        //    {
+        //        productTime = "Breakfast";
+        //        return productTime;
+        //    }
+        //    else if (RadioButtonDinner.IsChecked == true)
+        //    {
+        //        productTime = "Dinner";
+        //        return productTime;
+        //    }
+        //    else if (RadioButtonSupper.IsChecked == true)
+        //    {
+        //        productTime = "Supper";
+        //        return productTime;
+        //    }
+        //    else
+        //    {
+        //        productTime = "None";
+        //        return productTime;
+        //    }
+
+        //}
+        public void CountCalories()
+        {
+            decimal calories = 0;
+            
+            List<Products> breakfast_food = new List<Products>();
+            List<Products> dinner_food = new List<Products>();
+            List<Products> supper_food = new List<Products>();
+            foreach (var item in _breakfeastLbx.Items)
+                breakfast_food.Add((Products)item);
+            foreach (var item in _dinnerLbx.Items)
+                dinner_food.Add((Products)item);
+            foreach (var item in _supperLbx.Items)
+                supper_food.Add((Products)item);
+
+            foreach (var item in breakfast_food)
+            {
+                calories += item.Calories;
+                
+            }
+            foreach (var item in dinner_food)
+            {
+                calories += item.Calories;
+               
+            }
+            foreach (var item in supper_food)
+            {
+                calories += item.Calories;
+               
+            }
+
+            SumCalloriesTxtBl.Text = calories.ToString();
+            
+        }
+
     }
 }
